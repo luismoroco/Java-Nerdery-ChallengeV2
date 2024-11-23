4d477ba4fddf0221e5963e65cb0784400498a79e
lmoroco <lmorocoramos@gmail.com>
2024-11-22 19:11:16 -0500

    refactor: add earlier return

--- src/main/java/ExtraChallenges.java
+++ src/main/java/ExtraChallenges.java
@@ -10,29 +10,25 @@ import java.io.IOException;
 public class ExtraChallenges {
   final static String FILE_PATH = "weather.json";
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException, IllegalAccessException, NoSuchFieldException {
     var FILE = new File(FILE_PATH);
     var factory = new JsonFactory();
     var mapper = new ObjectMapper(factory);
 
     var meteorologyReport = new MeteorologyReport();
 
-    try {
-      var parser = factory.createParser(FILE);
-      if (parser.nextToken() == JsonToken.START_ARRAY) {
-        while (parser.nextToken() != JsonToken.END_ARRAY) {
-          var weather = mapper.readValue(parser, Weather.class);
-          if (!weather.verifyFields()) {
-            continue;
-          }
+    var parser = factory.createParser(FILE);
+    if (parser.nextToken() == JsonToken.START_ARRAY) {
+      return;
+    }
 
-          meteorologyReport.process(weather);
-        }
+    while (parser.nextToken() != JsonToken.END_ARRAY) {
+      var weather = mapper.readValue(parser, Weather.class);
+      if (!weather.verifyFields()) {
+        continue;
       }
 
-      meteorologyReport.show();
-    } catch (IOException | IllegalAccessException | NoSuchFieldException e) {
-      throw new RuntimeException(e);
+      meteorologyReport.process(weather);
     }
   }
 }
